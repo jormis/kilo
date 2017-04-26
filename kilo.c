3e6f493e39437da3b075b94aae9c8f1ad6bfa75c
jormis <jorma.korkiakoski@iki.fi>
2017-04-26 18:43:07 +0300

    Even more JavaScript (and HTML/Window) reserver words.

--- kilo.c
+++ kilo.c
@@ -357,7 +357,48 @@ char *JS_HL_keywords[] = {
         "var", "void|", "volatile",
         "while", "with", 
         "yield", 
+
+        /* And even more: JavaScript Object, Properties, and Methods. */
+        "Array|", "Date|", "hasOwnProperty", 
+        "Infinity|", "isFinite", "isNaN", "isPrototypeOf",
+        "length", "Math", "NaN|", "name", "Number|", "Object|",
+        "prototype", "String|", "toString", "undefined", "valueOf",
         
+        /* Java reserved words. */ 
+        
+        "getClass", "java", "JavaArray|", "javaClass", "JavaClass", 
+        "JavaObject|", "JavaPackage",
+        
+        /* Other reserved words. HTML and Window objects and properties. */
+
+        "alert", "all", "anchor", "anchors", "area", "assign", 
+        "blur", "button", 
+        "checkbox", "clearInterval", "clearTimeout", "clientInformation",
+        "close", "closed", "confirm", "constructor", "crypto",
+        "decodeURI", "decodeURIComponent", "defaultStatus", "document",
+        "element", "elements", "embed", "embeds", "encodeURI", 
+        "encodeURIComponent", "escape", "event", 
+        "fileUpload", "focus", "form", "forms", "frame", "frames", "frameRate",
+        "hidden", "history", 
+        "image", "images", "innerHeight", "innerWidth", 
+        "layer", "layers", "link", "location", 
+        "mimeTypes", 
+        "navigate", "navigator", 
+        "offscreenBuffering", "open", "opener", "option", "outerHeight", 
+        "outerWidth", 
+        "packages", "pageXOffset", "pageYOffset", "parent", "parseFloat", 
+        "parseInt", "password", "pkcs11", "plugin", "prompt", "propertyIsEnum",
+        "radio", "reset", 
+        "screenX", "screenY", "scroll", "secure", "select", "self", 
+        "setInterval", "setTimeout", "status", "submit", 
+        "taint", "text", "textarea", "top", "unescape", "untaint", 
+        "window",
+
+        /* HTML Event Handlers. */
+        
+        "onblur", "onclick", "onerror", "onfocus", "onkeydown", "onkeyup", 
+        "onmouseover", "onmouseup", "onmousedown", "onload", "onsubmit",
+                
         NULL
 };
 
